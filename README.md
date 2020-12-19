f5e424ddb817b6cd4056706daf142fc1bd1989e6
Sam Thorogood <sam.thorogood@gmail.com>
2020-12-19 12:54:11 +1100

    sents 0.3.0

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
+Command-line filesystem watcher, useful for running commands when something changes.
+Uses [sents](https://www.npmjs.com/package/sents) under the hood.
+
+# Usage
+
+```bash
+npm install sents-cli
+
+# for info on command-line args
+sents --help
+
+# to print all CSS files that have changed
+sents "foo/**/*.css"
+
+# to rebuild JS after any file changes, deduped to every second
+sents "**/*.js" -c "rollup" -d 1000
+```
+
+# Notes
+
+- This doesn't support polling, so don't use it on network filesystems, but you shouldn't really be doing interactive dev work from there anyway
+
+- You can't exclude files from this right now, so it might watch your whole `node_modules` folder
 
 # Dependencies
 
 All of these dependencies have zero further dependencies.
+Therefore, using this package will add at most three dependencies to your project.
 
 - `mri`: for parsing arguments
-- `picomatch`: supports glob syntax
+- `picomatch`: for supporting glob syntax
 - `sents`: the underlying watcher library
--- index.js
+++ index.js
@@ -8,11 +8,13 @@ import childProcess from 'child_process';
 
 const args = mri(process.argv.slice(2), {
   alias: {
+    'delay': ['d'],
     'debounce': ['d'],
     'command': ['c'],
     'root': ['r'],
   },
   default: {
+    'delay': undefined,
     'debounce': 400,
   },
   boolean: ['initial', 'help'],
@@ -28,12 +30,15 @@ otherwise prints matched changes.
 Globs:
   Uses \`picomatch\` for glob syntax. e.g., "src/**/*.js" or "blah/foo.css".
   If none specified, matches all files.
+  Be sure to specify your globs in quotes "*.js" so that your shell doesn't
+  expand them for you.
 
 Options:
   -d, --debounce <ms>  debounce time for command
   -c, --command <cmd>  command to run on change, use quotes
   -r, --root <path>    root path to use, or uses current working directory
   --dotfiles           whether to match dotfiles
+  --delay <ms>         tell sents to delay change detection by this long
 `;
   console.info(helpString);
   process.exit(0);
@@ -45,7 +50,7 @@ Options:
 const requestCommand = args.command ? (function() {
   const run = () => {
     // TODO(samthor): possibly broken
-    childProcess.execFileSync(args.command, {shell: true, stdio: 'inherit'});
+    childProcess.execSync(args.command, {shell: true, stdio: 'inherit'});
   };
 
   let timeoutCommand = 0;
@@ -76,15 +81,18 @@ const filter = (function() {
     return true;
   });
 
+  // TODO(samthor): Make noise about all the arguments which are not actually globs, so users
+  // realise that their shell might have expanded them.
+
   const globMatch = pm(globs, {
     dot: true,  // dotfiles are handled in watcher
   });
 
   return (s) => {
     if (s.endsWith(path.sep)) {
-      // TODO: do some kinda prefix match on left
+      // TODO(samthor): do some kinda prefix match on left
       // e.g. input: "foo/bar/" against
-      //      glob: "**/bar/zing/*.js" will pass
+      //      glob: "**/bar/zing/*.js" should pass
       return true;
     }
     return globMatch(s);
@@ -94,10 +102,15 @@ const filter = (function() {
 
 const root = args.root || process.cwd();
 
-const watcher = buildWatcher(root, {
+/** @type {import('sents').CorpusOptions} */
+const options = {
   dotfiles: Boolean(args.dotfiles),
   filter,
-});
+};
+if ('delay' in args) {
+  options.delay = +args.delay;
+}
+const watcher = buildWatcher(root, options);
 await watcher.ready;
 
 watcher.on('error', (e) => {
